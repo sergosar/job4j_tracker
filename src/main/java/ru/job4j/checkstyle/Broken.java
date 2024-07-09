f098d1f76143fe0c7e11fcc3295e8d503b4f9f53
SNMiroshnichenko <SNMiroshnichenko@vniief.ru>
2024-07-09 11:26:13 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,26 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    public String surname;
+    public static final String NEWVALUE = "";
+    String name;
+    private int sizeOfEmpty = 10;
+
+    Broken() {
+    }
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f) {
+
+    }
+
+}
