ca33a9d07d5bc79b1c49cb976e9470e813c86f12
sergosar <miroshnichenkos@mail.ru>
2024-07-20 12:18:05 +0300

    5. Tracker - хранилище [#396 #505919]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -23,7 +23,7 @@ public class Tracker {
     }
 
     public Item[] findByName(String key) {
-        var tempArr = new Item[100];
+        var tempArr = new Item[size];
         var index = 0;
         for (int i = 0; i < size; i++) {
             if (items[i].getName().equals(key)) {
