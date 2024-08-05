20f00488f46222cc88b73be6a0f560c237465f2e
SNMiroshnichenko <SNMiroshnichenko@vniief.ru>
2024-08-05 10:54:30 +0300

    12.1 Зависимость от System.out [#33568]

--- src/main/java/ru/job4j/tracker/FindByNameAction.java
+++ src/main/java/ru/job4j/tracker/FindByNameAction.java
@@ -1,6 +1,13 @@
 package ru.job4j.tracker;
 
 public class FindByNameAction implements UserAction {
+
+    private final Output output;
+
+    public FindByNameAction(Output output) {
+        this.output = output;
+    }
+
     @Override
     public String name() {
         return "Показать заявки по имени";
@@ -8,7 +15,7 @@ public class FindByNameAction implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        System.out.println("=== Вывод заявок по имени ===");
+        output.println("=== Вывод заявок по имени ===");
         String name = input.askStr("Введите имя: ");
         Item[] items = tracker.findByName(name);
         if (items.length > 0) {
@@ -16,7 +23,7 @@ public class FindByNameAction implements UserAction {
                 System.out.println(item);
             }
         } else {
-            System.out.println("Заявки с именем: " + name + " не найдены.");
+            output.println("Заявки с именем: " + name + " не найдены.");
         }
         return true;
     }
