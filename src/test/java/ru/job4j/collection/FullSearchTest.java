a5d16c576dac3c61ed43d6de038626cb9cd34c9f
sergosar <miroshnichenkos@mail.ru>
2024-08-13 17:07:17 +0300

    1. Уникальные задачи. [#212653]

---
+++ src/test/java/ru/job4j/collection/FullSearchTest.java
@@ -0,0 +1,70 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FullSearchTest {
+
+    @Test
+    public void whenExtractNumberWithDuplicates() {
+        FullSearch fullSearch = new FullSearch();
+        List<Task> tasks = Arrays.asList(
+                new Task("1", "First desc"),
+                new Task("2", "Second desc"),
+                new Task("1", "First desc")
+        );
+        Set<String> expected = new HashSet<>(Arrays.asList("1", "2"));
+        assertThat(fullSearch.extractNumber(tasks)).containsAll(expected);
+    }
+
+    @Test
+    public void whenExtractNumberWithUniqueTasks() {
+        FullSearch fullSearch = new FullSearch();
+        List<Task> tasks = Arrays.asList(
+                new Task("1", "First desc"),
+                new Task("2", "Second desc"),
+                new Task("3", "Third desc")
+        );
+        Set<String> expected = new HashSet<>(Arrays.asList("1", "2", "3"));
+        assertThat(fullSearch.extractNumber(tasks)).containsAll(expected);
+    }
+
+    @Test
+    public void whenExtractNumberWithEmptyList() {
+        FullSearch fullSearch = new FullSearch();
+        List<Task> tasks = List.of();
+        Set<String> expected = new HashSet<>();
+        assertThat(fullSearch.extractNumber(tasks)).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenExtractNumberWithAllTasksHavingSameNumber() {
+        FullSearch fullSearch = new FullSearch();
+        List<Task> tasks = Arrays.asList(
+                new Task("1", "First desc"),
+                new Task("1", "Second desc"),
+                new Task("1", "Third desc")
+        );
+        Set<String> expected = new HashSet<>(Arrays.asList("1"));
+        assertThat(fullSearch.extractNumber(tasks)).containsAll(expected);
+    }
+
+    @Test
+    public void whenExtractNumberWithNullTasks() {
+        FullSearch fullSearch = new FullSearch();
+        List<Task> tasks = Arrays.asList(
+                new Task("1", "First desc"),
+                null,
+                new Task("2", "Second desc"),
+                null
+        );
+        Set<String> expected = new HashSet<>(Arrays.asList("1", "2"));
+        assertThat(fullSearch.extractNumber(tasks)).containsAll(expected);
+    }
+}
