93d6a380285b34c676656605340ec5f8daf031ec
sergosar <miroshnichenkos@mail.ru>
2024-08-22 15:27:18 +0300

    0. Map, HashMap [#212652 #509995]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,8 +5,11 @@ import java.util.Map;
 
 public class UsageMap {
     public static void main(String[] args) {
-        Map<String, String> map = new HashMap();
+        Map<String, String> map = new HashMap<>();
         map.put("example@mail.ru", "Miroshnichenko Sergey Nikolaevich");
+        map.put("trulala@yandex.ru", "Сталин Иосиф Виссарионович");
+        map.put("example@mail.ru", "Новоеимя Можетбыть Любое");
+        map.put("prostopochta@pochta.ru", "Насклонегоры Облако Легкое");
         for (String key : map.keySet()) {
             System.out.println(key + " = " + map.get(key));
         }
