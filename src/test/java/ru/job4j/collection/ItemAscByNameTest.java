7e8e7467805006102cd87c91abb724cadcd90b5e
sergosar <miroshnichenkos@mail.ru>
2024-08-27 16:30:42 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/collection/ItemAscByNameTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.Item;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ItemAscByNameTest {
+    @Test
+    public void whenSortWithNaturalOrder() {
+        List<Item> items = new ArrayList<>(Arrays.asList(new Item("aaa"), new Item("bbb"), new Item("xxx"), new Item("ddd")));
+        items.sort(new ItemAscByName());
+        List<Item> expected = new ArrayList<>(Arrays.asList(new Item("aaa"), new Item("bbb"), new Item("ddd"), new Item("xxx")));
+        assertThat(items).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/ru/job4j/collection/ItemDescByNameTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.Item;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ItemDescByNameTest {
+    @Test
+    public void whenSortWithReverseOrder() {
+        List<Item> items = new ArrayList<>(Arrays.asList(new Item("aaa"), new Item("bbb"), new Item("xxx"), new Item("ddd")));
+        items.sort(new ItemDescByName());
+        List<Item> expected = new ArrayList<>(Arrays.asList(new Item("xxx"), new Item("ddd"), new Item("bbb"), new Item("aaa")));
+        assertThat(items).isEqualTo(expected);
+    }
+}
